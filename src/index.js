d65fe3d168ee227e15762b163d861848c1b537d8
Thomas-Hartmann <thomas@webtrade.dk>
2020-09-24 10:34:40 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import jokes from "./jokes";
+
+const allJokes = jokes.getJokes().map(joke => "<li>"+joke+"</li>");
+document.getElementById("jokes").innerHTML = allJokes.join("");
+document.getElementById("jokes").innerHTML = "HEHEHEHE";
+
+
+
+
