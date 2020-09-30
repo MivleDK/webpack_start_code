2b750292e85d3859170fb72464fbe29dcf0bdf6e
Thomas-Hartmann <thomas@webtrade.dk>
2020-09-30 15:55:17 +0200

    removed navigation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import jokes from "./jokes";
-import navigation from "./navigation";
+// import navigation from "./navigation";
 
 const allJokes = jokes.getJokes().map(joke => "<li>"+joke+"</li>");
 document.getElementById("jokes").innerHTML = allJokes.join("");
 
-// Solution 1
+// // Solution 1
+// document.getElementById('1').innerHTML = 'TEST11111111111111';
 
-
-// Solution 2
+// // Solution 2
+// document.getElementById('2').innerHTML = 'TEST2222222222222222';
 
 
--- src/navigation.js
+++ src/navigation.js
@@ -3,31 +3,40 @@
 const btnEx1 = document.getElementById('btnEx1');
 const btnEx2 = document.getElementById('btnEx2');
 const btnEx3 = document.getElementById('btnEx3');
+
 const sectionEx1 = document.getElementById('1');
 const sectionEx2 = document.getElementById('2');
 const sectionEx3 = document.getElementById('3');
 // const sectionEx4 = document.getElementById('4');
-const s1Content = sectionEx1.innerHTML;
-const s2Content = sectionEx2.innerHTML;
-const s3Content = sectionEx3.innerHTML;
+let s1Content = sectionEx1.innerHTML;
+let s2Content = sectionEx2.innerHTML;
+let s3Content = sectionEx3.innerHTML;
+const saveContent= ()=>{
+    s1Content = sectionEx1.innerHTML;
+    s2Content = sectionEx2.innerHTML;
+    s3Content = sectionEx3.innerHTML;
+}
 // const s4Content = sectionEx4.innerHTML;
 
 removeContent();
 
 btnEx1.onclick = (e)=>{
     e.preventDefault();
+    saveContent();
     removeContent();
     insertContent(1);
 }
 
 btnEx2.onclick = (e)=>{
     e.preventDefault();
+    saveContent();
     removeContent();
     insertContent(2);
 }
 
 btnEx3.onclick = (e)=>{
     e.preventDefault();
+    saveContent();
     removeContent();
     insertContent(3);
 }
