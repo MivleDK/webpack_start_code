d612c509903ebf817e3f87a0cabeea33f8efe89b
Thomas-Hartmann <thomas@webtrade.dk>
2020-10-01 08:53:14 +0200

    removed navigation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'
-// import jokes from "./jokes";
-import remove from "./navigation";
+import jokes from "./jokes";
 
-// const allJokes = jokes.getJokes().map(joke => "<li>"+joke+"</li>");
-// document.getElementById("jokes").innerHTML = allJokes.join("");
+const allJokes = jokes.getJokes().map(joke => "<li>"+joke+"</li>");
+document.getElementById("jokes").innerHTML = allJokes.join("");
 
-// Solution 1
-document.getElementById('1').innerHTML = 'TEST';
-
-// Solution 2
-document.getElementById('2').innerHTML = 'TEST2222222222222222';
-
-// Solution 3
-document.getElementById('3').innerHTML = 'TEST33333333333';
-
-remove();
